04a31044109a8c78c1b73938b37cf9f3e00f30f1
newton murimi <newtonmurimi82@gmail.com>
2025-07-15 15:57:25 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752584243854

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f814f4f0._.js",
+  "static/chunks/node_modules_f8dcaeb6._.js"
+],
+    source: "dynamic"
+});
